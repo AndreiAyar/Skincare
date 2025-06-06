2eb68d817d5ad25ab86db95100209ade19312cfd
AndreiAyar <49005196+AndreiAyar@users.noreply.github.com>
2025-06-06 15:35:34 +0000

    Remove commented-out code and redundant console logs to clean up the notification handling logic

--- client/src/controllers/NotificationsController.jsx
+++ client/src/controllers/NotificationsController.jsx
@@ -39,11 +39,8 @@ async function getiOSNotificationPermission() {
 }
 const cancelActiveNotifications = () => {
   try {
-    console.log("stergem odata");
     Notifications.cancelAllScheduledNotificationsAsync();
-  } catch (error) {
-    // console.log("Nu s-au putut sterge");
-  }
+  } catch (error) {}
 };
 let firstTrigger = false;
 let firstData = false;
@@ -85,13 +82,12 @@ const useNotification = () => {
       notificationHours &&
         notificationHours.map((hours) => {
           notificationBody.routines.map((notif, notificationIndex) => {
-            // console.log(notif)
+ 
             if (hours[notif._id]) {
-              //  console.log(hours[notif._id].morning_notification)
+   
               if (Object.keys(hours)[0] == notif._id) {
                 for (let i in notif.RoutineDetails) {
                   if (notif.RoutineDetails[i].partOfDay == "morning") {
- 
                     insertNotificationIntoDevice(
                       "Morning Notification",
                       `Time for your Morning Routine 🥰 ☀️ !`,
@@ -101,7 +97,6 @@ const useNotification = () => {
                     );
                   }
                   if (notif.RoutineDetails[i].partOfDay == "night") {
-  
                     insertNotificationIntoDevice(
                       "Night Notification",
                       `Time for your Night Routine 🥰 🌙 !`,
@@ -111,7 +106,6 @@ const useNotification = () => {
                     );
                   }
                   if (notif.RoutineDetails[i].partOfDay == "weekly") {
- 
                     insertNotificationIntoDevice(
                       "Weekly Notification",
                       `Time for your Weekly Routine 🥰 📅 !`,
@@ -123,55 +117,8 @@ const useNotification = () => {
                 }
               }
             }
-            // if (hours[notif._id]) {
-            //   if (hours[notif._id].custom_notification.time) {
-            //     // console.log(
-            //     //   `setam custom la ${
-            //     //     hours[notif._id].custom_notification.time
-            //     //   } pentru ${notif._id} cu produsele:`
-            //     // );
-            //     notif.RoutineDetails.map((details) => {
-            //       if (details.partOfDay == "weekly") {
-            //         //console.log(details)
-            //         console.log(' a intrat custom')
-            //         // insertNotificationIntoDevice(
-            //         //   "Weekly Notification",
-            //         //   `Time for your Weekly Routine 🥰 📅 !`,
-            //         //   notif._id,
-            //         //   details,
-            //         //   hours[notif._id].custom_notification.time
-            //         // );
-            //       }
-            //       if (hours[notif._id].morning_notification.time ) {
-            //         console.log(' a intrat morning')
-            //         console.log(notif._id)
-            //         console.log(Object.keys(hours)[0])
-            //        // console.log(hours)
-            //     //    console.log(details)
-            //         // insertNotificationIntoDevice(
-            //         //   "Morning Notification",
-            //         //   `Time for your Morning Routine 🥰 ☀️ !`,
-            //         //   notif._id,
-            //         //   details,
-            //         //   hours[notif._id].morning_notification.time
-            //         // );
-            //       }
-            //       if (hours[notif._id].night_notification.time) {
-            //         console.log(' a intrat night')
-            //         // insertNotificationIntoDevice(
-            //         //   "Night Notification",
-            //         //   `Time for your Night Routine 🥰 🌙 !`,
-            //         //   notif._id,
-            //         //   details,
-            //         //   hours[notif._id].night_notification.time
-            //         // );
-            //       }
-            //     });
-            //   }
           });
         });
-      //  });
-      //  });
     }
     firstNotif = true;
   }, [notificationBody]);
@@ -196,16 +143,16 @@ const useNotification = () => {
     );
     let adjustedTime = () => {
       let now = moment().format("YYYY-MM-DD HH:mm:ss");
-      console.log("acuuuuuuuuuu", now);
+ 
       let fromDB = moment(time).format("YYYY-MM-DD HH:mm:ss"); // ce vine din DB
-      console.log("din db", fromDB);
+ 
       let correctTime = time;
       if (fromDB < now) {
         let daysToAdd = moment(now).diff(fromDB, "days") + 1;
-        console.log(`+${daysToAdd} zile`);
+ 
 
         let tomorow = moment(correctTime).add(daysToAdd, "days").toDate(); //new Date(fromDB.getTime() + 1000 * 60 * 60 * 24);
-        console.log(moment(tomorow).toDate());
+ 
         correctTime = tomorow;
       }
       return new Date(correctTime).getTime();
@@ -276,72 +223,4 @@ const useNotification = () => {
 };
 
 export default useNotification;
-
-// const notificationsHandler = (notificationsData) => {
-//   // const [incomingNotifications, setIncomingNotifications] = useState({});
-//   // const [notificationBody, setNotificationBody] = useState(null);
-//   // const [currentRoutine, setCurrentRoutine] = useState();
-//  // const [getRoutines, { data, loading, called }] = useLazyQuery(ROUTINES_GQL);
-// console.log("MATA")
-//   // useEffect(() => {
-//   //   // getRoutines({
-//   //   //   variables: {
-//   //   //     filter: currentRoutine,
-//   //   //   },
-//   //   // });
-//   //   console.log('fired', notificationsData)
-
-//   // }, [notificationsData]);
-
-//   // const assignNotification = (notificationsData) => {
-//   //  setIncomingNotifications(notificationsData);
-
-//   //  incomingNotifications && incomingNotifications.activeNotifications&& console.log(incomingNotifications)
-
-//   //   getiOSNotificationPermission();
-//   //   cancelActiveNotifications();
-//   //   incomingNotifications.activeNotifications.map(({ routine_id }) => {
-//   //     //console.log('fa fetch la', routine_id)
-//   //     //!loading && setCurrentRoutine(routine_id);
-//   //     //!loading && data.routines && console.log(data.routines)
-//   //     //console.log("sarpili la fiecare", data.routines[0]._id);
-//   //   });
-
-//   // data &&
-//   //   data.routines &&
-//   //   console.log("sarpili la fiecare", data.routines[0]._id);
-//   // console.log("forta boss", notificationsData);
-//   // reFetch(fetch + 1);
-
-//   //   console.log(route_params);
-//   //  z.activeNotifications.map((notification) => {
-//   //    console.log(notification);
-
-//   // let NOTIF;
-//   // const localnotification = {
-//   //   title: `Time for your ${partOfTheDay} 🥰 !`,
-//   //   body,
-//   //   data: route_params,
-//   //   _displayInForeground: false,
-//   //   android: {
-//   //     sound: true,
-//   //   },
-//   //   ios: {
-//   //     sound: true,
-//   //   },
-//   // };
-//   // const schedulingOptions = {
-//   //   time,
-//   //   repeat: "day",
-//   // };
-
-//   // NOTIF = Notifications.scheduleLocalNotificationAsync(
-//   //   localnotification,
-//   //   schedulingOptions
-//   // );
-//   // return {
-//   //   assignNotification,
-//   // };
-// };
-
-// export default notificationsHandler;
+ 
